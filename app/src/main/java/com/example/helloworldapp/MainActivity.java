aa4656055c259837ef29fe1bdb1773aa319c1ecb
Jokubas Keturakis <jketura@gmail.com>
2024-03-05 23:18:35 +0200

    Added button which changes text view element's data

--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvText = findViewById(R.id.tvText);
+    }
+
+    public void buttonChangeTextClick(View view) {
+        this.tvText.setText("Hello and welcome!");
     }
 }
