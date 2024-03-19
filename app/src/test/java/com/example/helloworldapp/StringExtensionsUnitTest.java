6735247dd388d661b296c5ce1f7c5e72df94052e
Jokubas Keturakis <jketura@gmail.com>
2024-03-19 22:33:42 +0200

    remove comments

--- app/src/test/java/com/example/helloworldapp/StringExtensionsUnitTest.java
+++ app/src/test/java/com/example/helloworldapp/StringExtensionsUnitTest.java
@@ -6,15 +6,8 @@ import static org.junit.Assert.*;
 
 import com.example.helloworldapp.Utils.StringExtensions;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class StringExtensionsUnitTest {
 
-    // This test case should never hit the main flow
-    // since there is a check whether the input string string is empty prior to calling this method
     @Test
     public void CalculateWordCount_WhenInputStringIsEmpty_ReturnsZero(){
         final String inputString = "";
